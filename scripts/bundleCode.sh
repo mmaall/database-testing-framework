5c1d14bd6bec91d20b3ff19aa7e07646ac6f517c
Michael Lanthier <m2lanthier@gmail.com>
2020-03-13 10:43:42 -0700

    Fixed code bundling script. Now it works better

--- scripts/bundleCode.sh
+++ scripts/bundleCode.sh
@@ -19,17 +19,24 @@ mkdir load_test
 cd ../
 
 cp ../json-simple-1.1.1.jar $codeDir 
-cp ../mysql-connector-java-5.1.48-bin.jar $codeDir 
+cp ../mysql-connector-java-8.0.15.jar $codeDir 
 
 cp ../data_generation/UniqueIDGenerator.class ${codeDir}/data_generation/
+cp ../data_generation/InvalidValueException.class ${codeDir}/data_generation/
 cp ../load_test/LoadDriver.class ${codeDir}/load_test/
 cp ../load_test/DatabaseThread.class ${codeDir}/load_test/
 cp ../load_test/RecordInfo.class ${codeDir}/load_test/
-cp ../load_test/TransactionInfo.class ${codeDir}/load_test/ 
+cp ../load_test/TransactionInfo.class ${codeDir}/load_test/
+cp ../load_test/loadDB.sh ${codeDir}/load_test/
+cp ../.systemInfo ${codeDir}/
 
 # Zip it all together
 
 zip -r ${codeDir}.zip ${codeDir}/ 
+
+# Send it to s3
+aws s3 mv loadTestCode.zip s3://m2-rocks-db-test/loadTestCode.zip
+
 
 # Clean up after yourself you filthy animal
 rm -rf $codeDir/
